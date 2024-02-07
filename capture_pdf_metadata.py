14b0260b2ab650070042d9ee125ecd3a347b2996
unknown <fhb211129@fh-vie.ac.at>
2024-02-07 11:27:37 +0100

    exceptions eingebaut damit ungültige Werte nicht in die Liste übernommen werden

--- capture_pdf_metadata.py
+++ capture_pdf_metadata.py
@@ -8,7 +8,7 @@ logging.basicConfig(filename='logfile.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Verzeichnis mit den PDF-Dateien
-pdf_dir = r'C:\Users\jakob\Desktop\FHBFI PDF'
+pdf_dir = r'C:\Users\Jakob Moestl\Desktop\FH Technikum PDF'
 
 # Liste für die Metadaten
 metadata_list = []
@@ -17,22 +17,25 @@ metadata_list = []
 for filename in os.listdir(pdf_dir):
     if filename.endswith('.pdf'):
         pdf_path = os.path.join(pdf_dir, filename)
-        if filename.endswith('.pdf'):
-            print(filename)
+        try:
             # PDF-Datei lesen
             with open(pdf_path, 'rb') as f:
                 pdf = PdfReader(f)
                 # Metadaten extrahieren
                 metadata = pdf.metadata
-                print(metadata)
-                metadata_list.append(metadata)
+                if metadata is not None:
+                    metadata_list.append(metadata)
+                else:
+                    logging.warning(f'Keine Metadaten gefunden: {filename}')
+        except Exception as e:
+            logging.error(f'Fehler beim Lesen der Datei {filename}: {e}')
 
 # Metadaten in ein DataFrame konvertieren
 df = pd.DataFrame(metadata_list)
 
 # Speicherort der CSV-Datei auf deinem Desktop
 desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
-csv_file = os.path.join(desktop_path, 'metadaten.csv')
+csv_file = os.path.join(desktop_path, 'metadatenfhtechnikum.csv')
 
 # DataFrame in eine CSV-Datei exportieren
 df.to_csv(csv_file, index=False)
